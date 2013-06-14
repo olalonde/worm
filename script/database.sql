2364c801a6fb4bcf659a248831e8007514633829
Olivier Lalonde <olalonde@gmail.com>
2013-06-14 17:40:06 +0800

    commited test db schema

---
+++ script/database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users
+(
+   id SERIAL,
+   name_first character varying(255),
+   name_last character varying(255),
+   location character varying(255),
+   email character varying(255),
+   password character varying(255),
+   CONSTRAINT user_primary_key PRIMARY KEY (id)
+);
