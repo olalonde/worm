377ff1e76de3c7d9628e146326acfa3e50b6c4a5
Olivier Lalonde <olalonde@gmail.com>
2013-06-16 10:52:15 +0800

    test for selreferential lazy loading 1-to-1

--- test/level2one2one/lazy.js
+++ test/level2one2one/lazy.js
@@ -10,16 +10,18 @@ describe('lazy loading a 1-to-1 relationship', function () {
     common.pretest(done);
   });
 
-  var oli, passport;
+  var oli, $oli, passport, person = {
+    name: 'oli',
+    passport: {
+      code: 123,
+      country: 'Canada'
+    }
+  };
+
+  person.bestFriend = person;
 
   before(function (done) {
-    $.save($.wrap(Person, {
-      name: 'oli',
-      passport: {
-        code: 123,
-        country: 'Canada'
-      }      
-    })).end(function (err, res) {
+    $.save($.wrap(Person, person)).end(function (err, res) {
       if (err) return done(err);
       done();
     });
@@ -51,6 +53,28 @@ describe('lazy loading a 1-to-1 relationship', function () {
 
   it('person.passport_id should equal person.passport_id', function () {
     oli.passport_id.should.equal(oli.passport.id);
+  });
+
+  describe('lazy loading self-referential 1-to-1 relation ship', function () {
+    it('should not return an error', function (done) {
+      $.load($oli, 'bestFriend').end(function (err) {
+        should.ok(!err);
+        done();
+      });
+    });
+
+    it('person.bestFriend should exist', function () {
+      should.exist(oli.bestFriend);
+    });
+
+    it('person.bestfriend_id should equal person.bestFriend.id', function () {
+      oli.bestfriend_id.should.equal(oli.bestFriend.id);
+    });
+
+    it('person.bestfriend should equal person', function () {
+      oli.bestFriend.should.equal(oli);
+    });
+
   });
 });
 
