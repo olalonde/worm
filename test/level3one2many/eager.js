955c74a629093dcdf6c015505b8957d2010d138a
Olivier Lalonde <olalonde@gmail.com>
2013-06-17 18:11:53 +0800

    added test for eager loading hasMany + hasOne rels

--- test/level3one2many/eager.js
+++ test/level3one2many/eager.js
@@ -14,7 +14,10 @@ var post = {
     {
       text: 'second comment'
     }
-  ]
+  ],
+  author: {
+    name: 'Oli'
+  }
 }, $post = $.wrap(Post, post);
 
 describe('eager loading one-to-many relationship', function () {
@@ -69,6 +72,62 @@ describe('eager loading one-to-many relationship', function () {
     post.comments.forEach(function (comment, index) {
       res.comments[index].text.should.equal(comment.text);
     });
+  });
+
+});
+
+describe('eager loading one-to-many relationship + one-to-one relationship', function () {
+  var err, res;
+
+  before(function (done) {
+    $.cache.clear(done);
+  });
+
+  before(function (done) {
+    $.get(Post)
+      .where({ title: 'Some post...' })
+      .include(['comments', 'author'])
+      .end(function (_err, _res) {
+        err = _err;
+        res = _res;
+        done();
+      });
+  });
+
+  /**
+   * @TODO: fresh reload from db and check if changes are reflected
+   */
+
+  it('should not return an error', function () {
+    if (err) console.error(err);
+    should.not.exist(err);
+  });
+
+  it('post should have correct title', function () {
+    should.exist(res.title);
+    res.title.should.equal('Some post...');
+  });
+
+  it('post should have comments property set', function () {
+    should.exist(res.comments);
+  });
+
+  it('post should have 2 comments', function () {
+    res.comments.length.should.equal(2);
+  });
+
+  it('post should have 2 comments with the correct attributes', function () {
+    post.comments.forEach(function (comment, index) {
+      res.comments[index].text.should.equal(comment.text);
+    });
+  });
+
+  it('post should have an author', function () {
+    should.exist(post.author);
+  });
+
+  it('author name should be correct', function () {
+    post.author.name.should.equal('Oli');
   });
 
 });
